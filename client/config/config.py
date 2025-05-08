bfb5370d9e3705cbcaedcb22160a81c0a0768530
Korbinian Zormeier <Korbinian.Zormeier@fast-forest.de>
2025-05-08 23:12:51 +0200

    add data_validator and respective tests

--- client/config/config.py
+++ client/config/config.py
@@ -1,1 +1,6 @@
 file_location = ''
+
+backend_url = 'https://optimaloptimizer.thankfulbeach-22a876ac.westus2.azurecontainerapps.io/api/v1'
+backend_truck_drivers_url = f'{backend_url}/truck-drivers'
+backend_trailers_url = f'{backend_url}/trailers'
+backend_trucks_url = f'{backend_url}/trucks'
