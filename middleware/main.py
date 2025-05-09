ddf3d3babaffc8447fa16913e50a24205d38e444
tjorven - HP-9K5T3EA <burdorftjorven@gmail.com>
2025-05-09 10:27:48 +0200

    add CORS

--- middleware/main.py
+++ middleware/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Depends
+from starlette.middleware.cors import CORSMiddleware
 
 from middleware.routers.llm_router import router as llm_router
 from middleware.routers.speech_router import router as speech_router
@@ -15,6 +16,14 @@ app.include_router(speech_router, prefix="/v1/speech", tags=["Speech"], dependen
 app.include_router(llm_router, prefix="/v1/llm", tags=["LLM"], dependencies=[Depends(lambda: speech_service)])
 app.include_router(trip_router, prefix="/v1/trip", tags=["Trip"])
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
