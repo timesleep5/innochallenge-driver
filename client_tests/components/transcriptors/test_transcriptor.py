7ee15c9fd4e47adf0f4f5afbaddd5ae92af30572
Korbinian Zormeier <Korbinian.Zormeier@fast-forest.de>
2025-05-09 12:27:17 +0200

    add ent-to-end integration tests

--- client_tests/components/transcriptors/test_transcriptor.py
+++ client_tests/components/transcriptors/test_transcriptor.py
@@ -1,6 +1,7 @@
 import unittest
 
 from client.components.transcriptors.transcriptor import Transcriptor
+from utils import Utils
 
 
 class TestTranscriptor(unittest.TestCase):
@@ -8,17 +9,14 @@ class TestTranscriptor(unittest.TestCase):
         self.transcription_service_url = "http://127.0.0.1:8000/v1/speech/transcribe"
         self.transcriptor = Transcriptor(self.transcription_service_url)
 
-    def prepare_for_assert(self, string: str) -> str:
-        return string.lower().replace(",", "").replace(".", "").replace("'", "")
-
     def test_transcribe_bad_english_file_returns_consistent_transcription(self):
         file_path = "./../data/bad_test_english/iamdriverp5.wav"
         expected_transcription = "Hey, I'm driver P5 and I'm not feeling well today. I'm sorry but I can't take my shift."
-        expected_transcription = self.prepare_for_assert(expected_transcription)
+        expected_transcription = Utils.prepare_for_assert(expected_transcription)
 
         for _ in range(10):
             transcription = self.transcriptor.transcribe(file_path)
-            transcription = self.prepare_for_assert(transcription)
+            transcription = Utils.prepare_for_assert(transcription)
 
             self.assertEqual(transcription, expected_transcription)
 
@@ -33,7 +31,7 @@ class TestTranscriptor(unittest.TestCase):
         for path, expected_transcription in path_to_transcription.items():
             transcription = self.transcriptor.transcribe(path)
 
-            transcription = self.prepare_for_assert(transcription)
-            expected_transcription = self.prepare_for_assert(expected_transcription)
+            transcription = Utils.prepare_for_assert(transcription)
+            expected_transcription = Utils.prepare_for_assert(expected_transcription)
 
             self.assertEqual(transcription, expected_transcription)
